b4dc2a3f8dec532bc4ab46f421941fb84fbc2192
BrianWong05 <wongtiniaomacau@gmail.com>
2025-07-15 23:16:32 +0800

    feat: add language English

--- client-public/src/index.js
+++ client-public/src/index.js
@@ -3,17 +3,37 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { ConfigProvider } from 'antd';
 import zhTW from 'antd/locale/zh_TW';
+import enUS from 'antd/locale/en_US';
+import { useTranslation } from 'react-i18next';
 import App from './App';
 import './index.css';
 import './i18n';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <ConfigProvider locale={zhTW}>
+const AppWithLocale = () => {
+  const { i18n } = useTranslation();
+  
+  const getAntdLocale = () => {
+    switch (i18n.language) {
+      case 'en':
+        return enUS;
+      case 'zh-TW':
+      default:
+        return zhTW;
+    }
+  };
+
+  return (
+    <ConfigProvider locale={getAntdLocale()}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </ConfigProvider>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <AppWithLocale />
   </React.StrictMode>
 );
